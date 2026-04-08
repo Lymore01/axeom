66d0c17b397b3e8f36fec7057504a771308c0696
Lymore01 <kellytoroitich89@gmail.com>
2026-04-09 00:01:35 +0300

    feat: add `packages/axiom` as the main entry point

---
+++ packages/axiom/src/index.ts
@@ -0,0 +1,6 @@
+export * from "@axiom/core";
+export { createPinoLogger, type Logger } from "@axiom/logger";
+export { s, type Infer, type Validator } from "@axiom/schema";
+
+import { s } from "@axiom/schema";
+export default s;
